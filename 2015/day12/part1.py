1c31162673e9ce8fdfbb0f5b6ab7caf6210a8973
qbizzle68 <quinton.barnes68@gmail.com>
2025-12-06 20:38:41 -0600

    2015 day 12 (this was really easy)

---
+++ 2015/day12/part1.py
@@ -0,0 +1,39 @@
+import sys
+import json
+
+
+def count_numbers(data: dict | list | int) -> int:
+    # Base cases
+    if isinstance(data, int):
+        return data
+    if data == [] or data == {}:
+        return 0
+
+    if isinstance(data, list):
+        itr = iter(data)
+    elif isinstance(data, dict):
+        itr = iter(data.values())
+    else:
+        # I think the only other data type to worry about is a str, but either
+        # way we should just skip anything that cannot contain a nested structure
+        # instead of raising an exception
+        return 0
+        #raise Exception(f'data is of type {type(data)}')
+
+    sum = 0
+    for val in itr:
+        sum += count_numbers(val)
+
+    return sum
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print(f'Usage: {sys.argv[0]} JSONPATH')
+    filepath = sys.argv[1]
+
+    with open(filepath) as f:
+        data = json.load(f)
+
+    number_sum = count_numbers(data)
+    print(f'Total sum of numbers found is {number_sum}')
